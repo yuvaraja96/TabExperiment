a473b5581bbbc94876386c88a61d574bdb3c57e4
Yuvaraja <yuvarajaj-wa14@student.tarc.edu.my>
2019-04-16 08:47:21 +0800

    TabExperiment Practical

---
+++ app/src/main/java/com/example/android/tabexperiment/TabFragment1.java
@@ -0,0 +1,26 @@
+
+package com.example.android.tabexperiment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class TabFragment1 extends Fragment {
+
+    public TabFragment1() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.tab_fragment1, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/android/tabexperiment/TabFragment2.java
@@ -0,0 +1,28 @@
+
+package com.example.android.tabexperiment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class TabFragment2 extends Fragment {
+
+
+    public TabFragment2() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.tab_fragment2, container, false);
+    }
+
+}
